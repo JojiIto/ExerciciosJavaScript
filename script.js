638ae25c16e14d2d2ec847c27612a8ff4af523d1
João Guilherme <joaoguilherme2013@outlook.com.br>
2025-12-01 11:52:13 -0300

    exercicio

---
+++ script.js
@@ -0,0 +1,16 @@
+// Declare uma variável contendo uma string
+var nome = "João";
+// Declare uma variável contendo um número dentro de uma string
+var ano = "2003";
+// Declare uma variável com a sua idade
+var idade = 22;
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var sobrenome = "Ramos";
+var nomeCompleto = `${nome} + ${sobrenome}`;
+// Coloque a seguinte frase em uma variável: It's time
+var frase = `It's time`;
+
+// Verifique o tipo da variável que contém o seu nome
+var verificiarTipoNome = typeof nome;
+console.log(verificiarTipoNome);
