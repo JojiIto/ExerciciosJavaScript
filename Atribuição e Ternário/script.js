c144516c66912b3d4c6a242b4dba06568c5c21c6
João Guilherme <joaoguilherme2013@outlook.com.br>
2025-12-07 14:11:18 -0300

    att

---
+++ Atribuição e Ternário/script.js
@@ -0,0 +1,19 @@
+var numero = 20;
+var numero2 = 10;
+
+numero += numero2; // numero = numero + numero2
+console.log(numero);
+console.log(numero2);
+
+var idade = 22;
+var naoPossuiDiabetes = false;
+
+var podeBeber;
+podeBeber = idade >= 18 && naoPossuiDiabetes ? true : false;
+
+console.log(podeBeber);
+
+var possuiFaculdade = false;
+if (possuiFaculdade);
+console.log("sim possui");
+console.log("isso mesmo");
