fd4adc8f471eaa03a37b131497e5c794ec482a1f
João Guilherme <joaoguilherme2013@outlook.com.br>
2025-12-09 17:50:47 -0300

    fix

--- Classes e Atributos/script.js
+++ Classes e Atributos/script.js
@@ -1,3 +1,20 @@
-const menu = document.querySelector(".menu");
+// const menu = document.querySelector(".menu");
 
-console.log(menu.classList);
+// menu.classList.add("ativo", "teste");
+// menu.classList.remove("azul");
+// menu.classList.toggle("azul");
+
+// if (menu.classList.contains("azul")) {
+//   menu.classList.add("possui azul");
+// }
+// // console.log(menu.classList);
+
+const animais = document.querySelector(".animais");
+
+console.log(animais.attributes["data-texto"]);
+
+const img = document.querySelector("img");
+
+constsrcImg = img.getAttribute
+
+console.log()
--- ForEach e Arrow Function/script.js
+++ ForEach e Arrow Function/script.js
@@ -1,21 +1,22 @@
-// const imgs = document.querySelectorAll("img");
+const imgs = document.querySelectorAll("img");
 
-// imgs.forEach(function (item, index, array) {
-//   // console.log(item, index, array);
-// });
+imgs.forEach(function (item, index, array) {
+  // console.log(item, index, array);
+});
 
-// const titulos = document.getElementsByClassName("titulo");
-// const titulosArray = Array.from(titulos);
+const titulos = document.getElementsByClassName("titulo");
+const titulosArray = Array.from(titulos);
 
-// titulosArray.forEach(function (item, index, array) {
-//   // console.log(item, index, array);
-// });
+titulosArray.forEach(function (item, index, array) {
+  // console.log(item, index, array);
+});
 
-// const imgs = document.querySelectorAll("img");
+const item = document.querySelectorAll("img");
 
-// imgs.forEach((item) => {
-//   console.log(item);
-// });
+imgs.forEach((item) => {
+  console.log(item);
+});
 
 let i = 0;
 imgs.forEach((item) => console.log(i++));
+
